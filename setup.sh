2cc58d4231a0160af867046497fa0969825e013a
Jude <jm2489@njit.edu>
2024-10-16 22:47:12 -0400

    Added setup.sh to add it490.sh to $SHELL rc alias.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,28 @@
-# Basic setup to take all files needed to run the it490 script.
-mkdir /home/$USER/script
-cp * /home/$USER/script
-echo "Done."
+#!/usr/bin/bash
+
+# Adding it490.sh to alias in bashrc with the current working directory
+# This is so that the script can be run from anywhere
+add_alias() {
+    # Check to see what is the default shell
+    if [ -z "$SHELL" ]; then
+        echo "Error: SHELL environment variable not set."
+        exit 1
+    fi
+    # If using bash put in .bashrc if zsh put in .zshrc
+    if [[ "$SHELL" == "/usr/bin/bash" ]]; then
+        echo "Adding alias to .bashrc ..."
+        echo "alias it490='bash $PWD/it490.sh'" >> ~/.bashrc
+        source ~/.bashrc
+        echo "Alias added."
+    elif [[ "$SHELL" == "/usr/bin/zsh" ]]; then
+        echo "Adding alias to .zshrc ..."
+        echo "alias it490='bash $PWD/it490.sh'" >> ~/.zshrc
+        source ~/.zshrc
+        echo "Alias added."
+    else
+        echo "Error: Unsupported shell. I only care about two at the moment."
+        exit 1
+    fi
+}
+
+add_alias
